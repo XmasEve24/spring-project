ff037c0069b55de5800f75635ba3f6e65c5d8ee0
kobyunghyun <pbi911224@gmail.com>
2022-02-16 21:23:20 +0900

    Revert "마이페이지 수정"
    This reverts commit 9fd485ad73c01b9a4c4203a4f35d120ef44f06d3.

--- spring_project_2/src/main/java/com/test/app/member/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.test.app.member;
-
-public interface MemberService {
-	public MemberVO selectOne(MemberVO vo);
-		// ex) 로그인 기능
-	public void insertMember(MemberVO vo);
-	public void updateMember(MemberVO vo);
-	
-}
